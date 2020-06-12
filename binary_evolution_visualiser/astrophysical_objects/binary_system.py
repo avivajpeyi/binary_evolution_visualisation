b7dc0d8305a20692775df6e35980392cdb47970e
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2020-06-12 22:52:59 +1000

    add visualiser test

--- binary_evolution_visualiser/astrophysical_objects/binary_system.py
+++ binary_evolution_visualiser/astrophysical_objects/binary_system.py
@@ -1,8 +1,17 @@
+import numpy as np
+
 from .star import Star
 
 
 class BinarySystem:
-    def __init__(self, star_1: Star, star_2: Star):
+    def __init__(
+        self,
+        star_1: Star,
+        star_2: Star,
+        time: np.ndarray,
+        separation: np.ndarray,
+        eccentricity: np.ndarray,
+    ):
         """
 
         :param star_1:
@@ -10,6 +19,9 @@ class BinarySystem:
         """
         self.star_1 = star_1
         self.star_2 = star_2
+        self.time = time
+        self.separation = separation
+        self.eccentricity = eccentricity
 
     def __str__(self):
         return f"s1:{self.star_1}, s2:{self.star_2}"
--- binary_evolution_visualiser/visualiser.py
+++ binary_evolution_visualiser/visualiser.py
@@ -1,8 +1,12 @@
 """
 Visualises the evolution of a binary system
 """
+from .astrophysical_objects import BinarySystem
 
 
 class BinaryVisualiser:
-    def __init__(self):
-        pass
+    def __init__(self, binary_system: BinarySystem):
+        self.binary_system = binary_system
+
+    def render(self, outfile: str):
+        raise NotImplementedError
