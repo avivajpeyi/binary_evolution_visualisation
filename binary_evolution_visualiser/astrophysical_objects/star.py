21bd486c625af2436fdab183f5c16fee1e5eb4dd
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2020-06-12 21:35:22 +1000

    add stellar type template.

--- binary_evolution_visualiser/astrophysical_objects/star.py
+++ binary_evolution_visualiser/astrophysical_objects/star.py
@@ -2,8 +2,14 @@ import numpy as np
 
 
 class Star:
-    def __init__(self, mass: np.ndarray):
+    def __init__(
+        self, mass: np.ndarray, radius, stellar_type, luminosity, effective_temprature
+    ):
         self.mass = mass
+        self.radius = radius
+        self.stellar_type = stellar_type
+        self.luminosity = luminosity
+        self.effective_temprature = effective_temprature
 
     def __str__(self):
         return f"{self.mass}"
---
+++ binary_evolution_visualiser/astrophysical_objects/stellar_type.py
@@ -0,0 +1,6 @@
+from enum import Enum, unique
+
+
+@unique
+class StellarType(Enum):
+    raise NotImplementedError
